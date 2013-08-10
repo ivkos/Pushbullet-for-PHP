63b0e8180e4cbfd4659f1751e59130b382e7ea66
Ivaylo Stoyanov <ivko.mail@gmail.com>
2013-08-10 16:02:56 +0300

    Update examples after code cleanup

--- PushBullet.example.php
+++ PushBullet.example.php
@@ -9,7 +9,7 @@ try {
 
 
   #### LIST OF DEVICES ####
-  #### These methods return arrays containing device definitions, or NULL when no devices found
+  #### These methods return arrays containing device definitions, or NULL if there are no devices
   // Print an array containing all available devices (including other people's devices shared with you)
   print_r($p->getDevices());
 
@@ -20,26 +20,22 @@ try {
   print_r($p->getSharedDevices());
 
 
-  #### PUSHING TO ONE DEVICE ####
-  #### PushBullet::push() and its shorthand methods return true on success, or throw an exception on failure
+  #### PUSHING TO A SINGLE DEVICE ####
+  #### Methods return TRUE on success, or throw an exception on failure
   // Push to device 31337 a note with a title 'Hey!' and a body 'It works!'
-  $p->push(31337, 'note', 'Hey!', 'It works!');
   $p->pushNote(31337, 'Hey!', 'It works!');
 
   // Push to device 31337 a Google Maps address with a title 'Google HQ' and an address '1600 Amphitheatre Parkway'
-  $p->push(31337, 'address', 'Google HQ', '1600 Amphitheatre Parkway');
   $p->pushAddress(31337, 'Google HQ', '1600 Amphitheatre Parkway');
 
   // Push to device 31337 a to-do list with a title 'Shopping List' and items 'Milk' and 'Butter'
-  $p->push(31337, 'list', 'Shopping List', array('Milk', 'Butter'));
   $p->pushList(31337, 'Shopping List', array('Milk', 'Butter'));
 
   // Push to device 31337 a link with a title 'ivkos at GitHub' and a URL 'https://github.com/ivkos'
-  $p->push(31337, 'link', 'ivkos at GitHub', 'https://github.com/ivkos');
   $p->pushLink(31337, 'ivkos at GitHub', 'https://github.com/ivkos');
 
 
-  #### PUSHING TO MORE THAN 1 DEVICE ####
+  #### PUSHING TO MULTIPLE DEVICES ####
   // Push to all available devices
   $p->pushNote('all', 'Some title', 'Some text');
   
@@ -47,15 +43,12 @@ try {
   $p->pushList('my', 'Buy these', array('PHP for Dummies', 'New charger'));
   
   // Push to all devices shared with you
-  $p->pushAddress('shared', 'Lets meet here', 'The Lake, Central Park, NY');
+  $p->pushAddress('shared', "Let's meet here", 'The Lake, Central Park, NY');
   
-  // Push to multiple devices (defined by their IDs)
+  // Push to multiple devices defined by their IDs
   // When pushing to multiple device IDs, some of them might fail. If so, an exception saying
-  // which devices failed will be thrown. If a device ID isn't in the message, it means push is successful.
+  // which devices have failed will be thrown. If a device ID isn't in the message, it means push is successful.
   $p->pushLink(array(1234, 1337, 2013), 'Check this out!', 'http://youtu.be/dQw4w9WgXcQ');
-  
-  // You can use the PushBullet:push() method likewise
-  $p->push('all', 'note', 'Some title', 'Some text');
 }
 catch (PushBulletException $e) {
   // Exception handling
