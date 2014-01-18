f2feda50f0e1a996d4444e37a1679ee9f531ff7c
Ivaylo Stoyanov <ivko.mail@gmail.com>
2014-01-18 18:18:28 +0200

    Update examples

--- PushBullet.example.php
+++ PushBullet.example.php
@@ -8,9 +8,12 @@ try {
   $p = new PushBullet('YOUR_API_KEY');
 
 
+
   #### LIST OF DEVICES ####
-  #### These methods return arrays containing device definitions, or NULL if there are no devices
-  // Print an array containing all available devices (including other people's devices shared with you)
+  #### These methods return arrays containing device definitions, or NULL if there are no devices.
+  #### Use them to get 'device_iden' which is a unique ID for every device and is used with push methods below.
+  
+  // Print an array containing all available devices (including other people's devices shared with you). 
   print_r($p->getDevices());
 
   // Print an array with your own devices
@@ -20,22 +23,26 @@ try {
   print_r($p->getSharedDevices());
 
 
+
   #### PUSHING TO A SINGLE DEVICE ####
-  #### Methods return TRUE on success, or throw an exception on failure
-  // Push to device 31337 a note with a title 'Hey!' and a body 'It works!'
-  $p->pushNote(31337, 'Hey!', 'It works!');
+  #### Methods return TRUE on success, or throw an exception on failure.
+  
+  // Push to device s2GBpJqaq9IY5nx a note with a title 'Hey!' and a body 'It works!'
+  $p->pushNote('s2GBpJqaq9IY5nx', 'Hey!', 'It works!');
 
-  // Push to device 31337 a Google Maps address with a title 'Google HQ' and an address '1600 Amphitheatre Parkway'
-  $p->pushAddress(31337, 'Google HQ', '1600 Amphitheatre Parkway');
+  // Push to device a91kkT2jIICD4JH a Google Maps address with a title 'Google HQ' and an address '1600 Amphitheatre Parkway'
+  $p->pushAddress('a91kkT2jIICD4JH', 'Google HQ', '1600 Amphitheatre Parkway');
 
-  // Push to device 31337 a to-do list with a title 'Shopping List' and items 'Milk' and 'Butter'
-  $p->pushList(31337, 'Shopping List', array('Milk', 'Butter'));
+  // Push to device qVNRhnXxZzJ95zz a to-do list with a title 'Shopping List' and items 'Milk' and 'Butter'
+  $p->pushList('qVNRhnXxZzJ95zz', 'Shopping List', array('Milk', 'Butter'));
 
-  // Push to device 31337 a link with a title 'ivkos at GitHub' and a URL 'https://github.com/ivkos'
-  $p->pushLink(31337, 'ivkos at GitHub', 'https://github.com/ivkos');
+  // Push to device gXVZDd2hLY6TOB1 a link with a title 'ivkos at GitHub' and a URL 'https://github.com/ivkos'
+  $p->pushLink('gXVZDd2hLY6TOB1', 'ivkos at GitHub', 'https://github.com/ivkos');
+
 
 
   #### PUSHING TO MULTIPLE DEVICES ####
+  
   // Push to all available devices
   $p->pushNote('all', 'Some title', 'Some text');
   
@@ -48,7 +55,7 @@ try {
   // Push to multiple devices defined by their IDs
   // When pushing to multiple device IDs, some of them might fail. If so, an exception saying
   // which devices have failed will be thrown. If a device ID isn't in the message, it means push is successful.
-  $p->pushLink(array(1234, 1337, 2013), 'Check this out!', 'http://youtu.be/dQw4w9WgXcQ');
+  $p->pushLink(array('5ZUC8hKOqLU0Jv7', 'SwZLKGn6M0fe7tO', 'tc7JSzKJLGLcKII'), 'Check this out!', 'http://youtu.be/dQw4w9WgXcQ');
 }
 catch (PushBulletException $e) {
   // Exception handling
