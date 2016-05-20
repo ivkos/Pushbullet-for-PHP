f6c71575486af2e23bd5d3e60d9c75c8266b6c91
Ivaylo Stoyanov <me@ivkos.com>
2016-05-20 07:40:44 -0700

    Add DeprecatedException

---
+++ src/Pushbullet/Exceptions/DeprecatedException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Pushbullet\Exceptions;
+
+/**
+ * Thrown if a deprecated action is executed
+ *
+ * @package Pushbullet\Exceptions
+ */
+class DeprecatedException extends PushbulletException
+{
+
+}
